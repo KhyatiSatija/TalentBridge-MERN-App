28424673004a16f683a4182a38c692542f6eb1cf
KhyatiSatija <satijakhyati2003@gmail.com>
2025-02-25 18:14:45 +0530

    URL

--- frontend/src/pages/Developer/Applications.js
+++ frontend/src/pages/Developer/Applications.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState, useCallback, useRef} from "react";
 import api from '../../api';
 import "../../tailwind.css"; 
 import "../../assets/css/Developer/Applications.css";
-import { FaEllipsisV, FaTrashAlt, FaRedo, FaBookmark, FaTimes  } from "react-icons/fa";
+import { FaEllipsisV } from "react-icons/fa";
 import Header from "../../components/Header";
 
 const Applications = () => {
