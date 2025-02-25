9ae388ccff57292cdfa0fe7ad539f6e17efd443b
KhyatiSatija <satijakhyati2003@gmail.com>
2025-02-25 18:17:17 +0530

    URL Update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL_PROD,
+  baseURL: process.env.REACT_APP_BACKEND_URL_PROD || 'http://localhost:5000',
 });
 
 export default api;
